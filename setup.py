a448529c113f8aeae733390976ade31fd7c76ee6
Rocky Feng <folowing@gmail.com>
2017-12-11 20:41:59 +0800

    Fix: bump to 0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='aiowx',
-    version='0.1',
+    version='0.2',
     description='Asyncio-based client for Wei Xin Platform',
     author='Rocky Feng',
     author_email='folowing@gmail.com',
