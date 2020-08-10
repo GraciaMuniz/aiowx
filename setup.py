e7f850cd49c077577f45714ef218c9862f36c3f3
Rocky Feng <folowing@gmail.com>
2020-08-10 19:11:22 +0800

    Fix: python version, aiohttp verion, and bump version

--- setup.py
+++ setup.py
@@ -3,12 +3,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='aiowx',
-    version='0.5',
+    version='0.7',
     description='Asyncio-based client for Wei Xin Platform',
     author='Rocky Feng',
     author_email='folowing@gmail.com',
     packages=find_packages(),
     include_package_data=True,
-    install_requires=['aiohttp>=3.0.0'],
+    install_requires=['aiohttp>=3.6.2'],
+    python_requires='>3.8.0',
     zip_safe=False,
 )
